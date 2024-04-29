0feefb7ba16fcfeb04c3e8e657b1a1240c5bc8d0
cool <159523059+techguru0411@users.noreply.github.com>
2024-04-29 18:27:21 +1000

    complete static pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,3 +11,32 @@ $(".__sp-item").click(function() {
 $(".fv_taparea").click(function() {
     $(".tapped_colorballs").toggle();
 })
+
+
+ScrollReveal().reveal('.fade_left', { 
+    duration: 1600, 
+    origin: 'left', 
+    distance: '50px',
+    // reset: true   
+  });
+
+ScrollReveal().reveal('.fade_right', { 
+    duration: 1600, 
+    origin: 'right', 
+    distance: '50px',
+    // reset: true   
+  });
+
+  ScrollReveal().reveal('.fade_bottom_ttl', { 
+    duration: 1500, 
+    origin: 'bottom', 
+    distance: '50px',
+    // reset: true   
+  });
+
+ScrollReveal().reveal('.fade_bottom_content', { 
+    duration: 2500, 
+    origin: 'bottom', 
+    distance: '50px',
+    // reset: true   
+});
